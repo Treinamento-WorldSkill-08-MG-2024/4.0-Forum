a9dfc38f7435072b32e41da47a4f4380ec3ac25e
Gabriel-Spinola <gabrielspinola77@gmail.com>
2024-06-17 14:43:46 -0300

    página de registro

--- application/lib/modules/auth_modules.dart
+++ application/lib/modules/auth_modules.dart
@@ -23,6 +23,10 @@ class UserModel {
       _ => throw const FormatException("Fail to convert json to user model")
     };
   }
+
+  Map<String, dynamic> toJson() {
+    return {'name': name, 'email': email, 'password': password};
+  }
 }
 
 class UserModelFields {
@@ -38,28 +42,31 @@ class AuthHandler {
       headers: {'Content-Type': 'application/json'},
       body: jsonEncode({'name': email, 'password': password}),
     );
+    
 
     final bodyData = jsonDecode(response.body) as Map<String, dynamic>;
-    assert(bodyData.containsKey("message"), "Login response does not contain message key");
+    assert(bodyData.containsKey("message"),
+        "Login response does not contain message key");
 
-    final data = bodyData['message'];
+    final data = bodyData['message'] as Map<String, dynamic>;
     if (response.statusCode != 200) {
       if (kDebugMode) {
-        print(data['message']);
+        print(data);
       }
 
-      throw Exception("Falha ao fazer login");
+      throw Exception(data);
     }
 
-    final prefs = await SharedPreferences.getInstance();
-
-    assert(data.containsKey("token"), "Login response does not contain token key");
-    assert(data.containsKey("user"), "Login response does not contain user key");
-
+    assert(
+        data.containsKey("token"), "Login response does not contain token key");
+    assert(
+        data.containsKey("user"), "Login response does not contain user key");
     final token = data["token"] as String;
-    await prefs.setString(UserModelFields.token, data['user']);
 
-    return UserModel.fromJson(data, token: token);
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(UserModelFields.token, data['token']);
+
+    return UserModel.fromJson(data['user'], token: token);
   }
 
   Future<bool> isAuthenticated() async {
@@ -72,9 +79,13 @@ class AuthHandler {
     throw Exception("Not Implements");
   }
 
-  Future<bool> register() async {
-    final response =
-        await http.Client().get(Uri.parse("$_kBaseURL/register"));
+  Future<bool> register(UserModel userData) async {
+    final response = await http.Client().post(
+      Uri.parse("$_kBaseURL/register"),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode(userData.toJson()),
+    );
+
     if (response.statusCode != 201) {
       throw Exception("Falha ao registrar usuário");
     }
--- application/lib/screens/auth/register_screen.dart
+++ application/lib/screens/auth/register_screen.dart
@@ -1,3 +1,6 @@
+import 'package:application/components/toats.dart';
+import 'package:application/modules/auth_modules.dart';
+import 'package:application/screens/auth/login_screen.dart';
 import 'package:flutter/material.dart';
 
 class RegisterScreen extends StatefulWidget {
@@ -8,8 +11,83 @@ class RegisterScreen extends StatefulWidget {
 }
 
 class _RegisterScreenState extends State<RegisterScreen> {
+  final _formkey = GlobalKey<FormState>();
+  final _nameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  final _authHandler = AuthHandler();
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        leading: BackButton(onPressed: () => Navigator.of(context).pop()),
+      ),
+      body: Center(
+        child: Form(
+          key: _formkey,
+          child: Column(
+            children: [
+              TextFormField(
+                controller: _nameController,
+              ),
+              TextFormField(
+                controller: _emailController,
+              ),
+              TextFormField(
+                controller: _passwordController,
+              ),
+              OutlinedButton(onPressed: _onSubmit, child: const Text("Entrar")),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _onSubmit() async {
+    _formkey.currentState!.save();
+    if (!_formkey.currentState!.validate()) {
+      // TODO -
+      return;
+    }
+
+    try {
+      final ok = await _authHandler.register(UserModel(
+        _nameController.text,
+        _emailController.text,
+        _passwordController.text,
+      ));
+
+      if (!ok) {
+        throw Exception("Tente novamente mais tarde");
+      }
+
+      if (!mounted) {
+        return;
+      }
+
+      await showDialog(
+          context: context,
+          builder: (_) => Toasts.successDialog("Conta criado com sucesso"));
+
+      if (!mounted) {
+        return;
+      }
+
+      Navigator.of(context)
+          .push(MaterialPageRoute(builder: (_) => const LoginScreen()));
+    } catch (error) {
+      if (!mounted) {
+        return;
+      }
+
+      showDialog(
+        context: context,
+        builder: (_) => Toasts.failureDialog(
+            "Houve um erro ao cadastrar seu usuário: $error"),
+      );
+    }
   }
 }
